f6798819f67dc9fd1b42899065932db0ef3b4bfd
Ken Santos <ken.santos919@gmail.com>
2025-01-20 19:11:16 -0600

    Initial

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: { domains: ["coin-images.coingecko.com"] },
+};
+
+module.exports = nextConfig;
+
---
+++ src/mock/chart-data.ts
@@ -0,0 +1,4 @@
+export const chartData = [
+  { date: "2024-04-01", usd: 222, btc: 150 },
+  
+];
